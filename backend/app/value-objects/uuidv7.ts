f37ff348b32ef03470285b647bfc994a178687e1
localusercamp <user062199@gmail.com>
2025-08-28 11:06:40 +0500

    chore: uuidv7

--- backend/app/value-objects/uuidv7.ts
+++ backend/app/value-objects/uuidv7.ts
@@ -2,13 +2,13 @@ import { ValueObject } from "../foundation/value-object";
 
 import { z } from "zod";
 
-class UUIDv4 extends ValueObject<string> {
+class UUIDv7 extends ValueObject<string> {
     public validate(value: string): boolean {
-        return z.uuidv4().safeParse(value).success;
+        return z.uuidv7().safeParse(value).success;
     }
 }
 
 export {
-    UUIDv4,
+    UUIDv7,
 };
 
